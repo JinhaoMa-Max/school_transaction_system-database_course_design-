a0acbf7a33bf7d5c4385f4358b803c3b53ef3125
Max <1733963368@qq.com>
2026-07-07 08:23:45 +0800

    后端保留规范版本，删掉之前有点乱的版本

--- backend/CampusSecondHand.Api/Services/Interfaces/IGoodsService.cs
+++
@@ -1,9 +0,0 @@
-using CampusSecondHand.Api.Models.Dtos;
-
-namespace CampusSecondHand.Api.Services.Interfaces;
-
-public interface IGoodsService
-{
-    Task<GoodsListResult> GetListAsync(int page, int size);
-    Task<GoodsDto?> GetByIdAsync(int id);
-}
